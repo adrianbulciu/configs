232dae409d8c66026dafa0787f9a226a455036c9
Bulciu Adrian <bulciu.adrian@gmail.com>
2025-02-05 10:26:21 +0200

    added Go Lang LSP, added MEH keybind for window resizing shortcut

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -7,6 +7,10 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 -- PHP
 require('lspconfig').intelephense.setup({ capabilities = capabilities })
 
+require('lspconfig').gopls.setup({ capabilities = capabilities })
+
+require('lspconfig').golangci_lint_ls.setup({ capabilities = capabilities })
+
 -- Vue, JavaScript, TypeScript
 require('lspconfig').volar.setup({
   capabilities = capabilities,
